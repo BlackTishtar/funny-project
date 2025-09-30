25ec44b82abda0c6b82e91aa1b877fc90bf8a73b
blacktishtar <blacktishtar@proton.me>
2025-09-30 18:18:58 +0330

    Euler problem-7 solved

---
+++ Euler/problem-7.c
@@ -0,0 +1,33 @@
+/*
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    What is the 10001st prime number?
+*/
+//##################################
+
+#include <stdio.h>
+
+int is_prime(int);
+
+int main() {
+    int count = 0;
+    int n = 0;
+
+    while (count <= 10001) {
+        n++;
+        if (is_prime(n))
+            count++;
+    }
+
+    printf("%d\n", n);
+    return 0;    
+}
+
+int is_prime(int n) {
+    if ( n <= 1) return 0;
+    
+    for (int i = 2; i * i < n; i++) 
+        if (n % i == 0)
+            return 0;
+
+    return 1;
+}
